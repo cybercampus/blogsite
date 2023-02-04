05e1b7280b530d222057a3547c3511ee0f7805e9
cybercampus <ligf_min@126.com>
2023-02-04 20:42:57 +0800

    login & register with block - base.html

--- users/forms.py
+++ users/forms.py
@@ -21,6 +21,9 @@ class LoginForm(forms.Form):
 
 # 使用 Django 提供的 ModelForm 实现注册页面
 class RegisterForm(forms.ModelForm):
+    username = forms.CharField(label='用户名', max_length=32, widget=forms.TextInput(attrs={
+        'class':'input', 'placeholder':'用户名'
+    }))
     password = forms.CharField(label='密码', max_length=6, widget=forms.PasswordInput(attrs={
         'class':'input', 'placeholder':'密码'
     }))
